c0fb2a5e2267ff62bfb3c7ad5c8eddc59362576e
cyf <625019880@qq.com>
2020-07-09 16:42:29 +0800

    迁移数据连接断开，修改下标继续迁移数据

--- src/test/java/com/finfosoft/test/FixedThreadPool.java
+++ src/test/java/com/finfosoft/test/FixedThreadPool.java
@@ -2,6 +2,13 @@ package com.finfosoft.test;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -21,5 +28,20 @@ public class FixedThreadPool {
         }
         long l1 = System.currentTimeMillis();
         System.out.println(l1 - l);
+    }
+
+    @Test
+    public void getInedx() throws Exception {
+//        BufferedReader bufRead = new BufferedReader(new FileReader("E:\\work\\data_config_t.csv"));
+//        ArrayList dataIdList = new ArrayList<Integer>();
+//        String line;
+//        while ((line = bufRead.readLine()) != null) {
+//            int dataId = Integer.parseInt(line.replace("\"", ""));
+//            dataIdList.add(dataId);
+//        }
+//        System.out.println(dataIdList);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = format.parse("2019-5-13 10:9:24");
+        System.out.println(date);
     }
 }
